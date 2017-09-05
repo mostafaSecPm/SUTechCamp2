3141da81471266f3849bfe352f39b2a70afd6921
m.pourshaker <m.pourshaker@evoteam.ir>
2017-09-05 15:59:33 +0430

    forth session

---
+++ app/src/main/java/evoteam/ir/example/DataBaseSchema.java
@@ -0,0 +1,25 @@
+package evoteam.ir.example;
+
+/**
+ * Created by programmer on 05-Sep-17.
+ */
+
+public class DataBaseSchema {
+
+    public static class TaskDataBase {
+        public static final String TABLE_NAME = "Tasks" ;
+
+        public class cols {
+            public static final String NAME = "name" ;
+            public static final String DAY = "day" ;
+            public static final String DATE = "date" ;
+            public static final String TIME = "time" ;
+            public static final String DESC = "desc" ;
+            public static final String IMPORTANCE = "importance" ;
+
+
+        }
+        
+        
+    }
+}
